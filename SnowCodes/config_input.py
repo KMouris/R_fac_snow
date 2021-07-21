78ba2b66b73c941d9cf6b98d117b7c8ca0b0db07
Maria Fernanda Morales Oreamuno <oreamuno@iws.uni-stuttgart.de>
2021-07-21 14:14:47 +0200

    config file docstring modification

--- SnowCodes/config_input.py
+++ SnowCodes/config_input.py
@@ -32,22 +32,21 @@ except ModuleNotFoundError as e:
           'pandas, rasterstats, math, scipy, tqdm')
     print(e)
 
-# sys.path.extend([f'./{name}' for name in os.listdir(".") if os.path.isdir(name)])
 
 """Boolean variables to determine which modules to run and which input parameters to include: 
-    - run_pt_manipulation: 'True' to run precipitation and temperature manipulation code to obtain the .csv files which 
-    are used in "rain_snow_rasters" to determine precipitation and snow rasters. 'False' to directly input the .csv 
-    files with the temperature and precipitation values for each raster cell. 
+    - run_pt_manipulation: 'True' to run 'pt_raster_manipulation' code to obtain the .csv files which are used in 
+        "rain_snow_rasters.py" to determine precipitation and snow rasters. 'False' to directly input the .csv 
+        files with the temperature and precipitation values for each raster cell. 
     - run_ero_snow: 'True' to run Ero_Sno program to calculate rain and snow rasters, 'False' to input the snow and/or 
-    precipitation rasters directly.
-    - run_snow_cover: 'True' to run snow_cover code, 'False' to directly input the binary snow raster, to calculate 
-    areas of snow from satellite images.
+        precipitation rasters directly.
+    - run_snow_cover: 'True' to run 'snow_cover.py' code, 'False' to directly input the binary snow raster, to calculate 
+        areas of snow cover from satellite images.
     - run_satellite_image_clip_merge: 'True' to run si_clip_merge.py and merge and clip the raw input satellite images. 
-    'False' if user inputs previously clipped and merged satellite images. 
+        'False' if user sets previously clipped and merged satellite images. 
     - run_snow_melt: 'True' to run the python programs to calculate the snow at the end of the month and the total snow 
-    melt from the snow detection and snow rasters. 
-    - run_r_factor: 'True' to run the python program to calculate the R(ain) factor for the 
-    RUSLE model, from the rain rasters.
+        melt from the snow detection and snow rasters. 
+    - run_r_factor: 'True' to run the python program to calculate the R(ain) factor for the RUSLE model, from the rain 
+        rasters.
     - run_total_factor: 'True' to calculate the total precipitation factor (rain + snow) """
 run_pt_manipulation = False
 run_rain_snow_rasters = False
@@ -57,17 +56,14 @@ run_snow_melt = True
 run_r_factor = True
 run_total_factor = False
 
-"""
-Files needed regardless of which codes to run: 
-    - snapraster_path: Path (including folder location, name and extension (must be .tif) with the raster to use as snap 
-    (obtain projection, extension and raster resolution). It is usually DEM raster file.
-    - shape_path = path (including folder location and name.shp) of shapefile to clip rasters with which to clip the 
-    resulting raster with.
+""" Files needed regardless of which codes to run: 
+    - snapraster_path: path (including name.ext) of raster with which to snap (obtain projection, extension and raster 
+        resolution). It is usually DEM raster file.
+    - shape_path = path (including folder location and name.shp) of shape file with which to clip resulting rasters 
     - results path: path where to save all results.
-    - start_date, end_date: in YYYYMM format, determines the time interval to analyze for. If they are the same, only 
-    one month will be analyzed and run_snow_melt will automatically turn to "False".
-    - geo_utils_path: path where geoutils package is located.
-"""
+    - start_date, end_date: string in YYYYMM format, determines the time interval to analyze for. If they are the same, 
+        only one month will be analyzed and run_snow_melt will automatically turn to "False".
+    - geo_utils_path: path where geoutils package is located."""
 snapraster_path = r'' + os.path.abspath('..\Input\DEM\\fildemBanja.tif')
 shape_path = r'' + os.path.abspath('..\Input\Shapes\\totalboundary.shp')
 results_path = r'' + os.path.abspath('../Results')
@@ -77,27 +73,23 @@ end_date = '201804'
 
 path_geo_utils = r'Y:\Abt1\hiwi\Oreamuno\Tasks\Snow_Codes\geo-utils/'
 
-"""
-If run_pt_manipulation = 'True'
-    - precipitation_path: folder path which contains .txt ASCII rasters with the precipitation data values for each time 
-    interval. Each .txt file corresponds to the data for the given year-month-day-hour. The file name corresponds to the 
-    date in the format YYYYMMDD_0HH.
-    - temperature_path: folder path which contains .txt ASCII rasters with the temperature data values for each time 
-    interval. Each .txt file corresponds to the data for the given year-month-day-hour. The file name corresponds to the 
-    date in the format YYYYMMDD_0HH.
-"""
+"""If run_pt_manipulation = 'True'
+- precipitation_path: folder path which contains .txt ASCII rasters with the precipitation data values for each time 
+    interval. Each .txt file corresponds to the data for the given year-month-day-hour. The file name corresponds to 
+    the date in the format YYYYMMDD_0HH.
+- temperature_path: folder path which contains .txt ASCII rasters with the temperature data values for each time 
+     interval. Each .txt file corresponds to the data for the given year-month-day-hour. The file name corresponds to 
+    the date in the format YYYYMMDD_0HH."""
 precipitation_path = r'' + os.path.abspath('../Input/Precipitation_Data/Precip_Banja_hourly_noCorr')
 temperature_path = r'' + os.path.abspath('../Input/Temperature_Data')
 
-"""
-If run_pt_manipulation = 'False' (AND run_rain_snow_rasters = True)
-    - PT_path: Path of folder with precipitation and Temperature data, for each cell, for time span to analyze. 
-    Each file is a .csv file corresponding to the row_column of each cell. Each row in the array is a different 
-    year-month-day-hour. The format of the column is year-month-day-hour-minute-second-Precipitation-Temperature-Row
-    -Column.
-    -  Original raster information: Number of columns, number of rows, coordinates of lower left corner (xllcorner and 
-        yllcorner), cellsize (cell resolution) and nodata value.
-"""
+"""If run_pt_manipulation = 'False' (AND run_rain_snow_rasters = True)
+- PT_path: folder path with precipitation and Temperature data. There must be a folder each month in the analysis 
+    time span. Each folder contains .csv files for each cell in the final raster, whose name corresponds to 
+    row_column of each cell. Each row in the array is a different year-month-day-hour. The format of the column is 
+    year-month-day-hour-minute-second-Precipitation-Temperature-Row-Column.
+-  Original raster information: Number of columns, number of rows, coordinates of lower left corner (xllcorner and 
+    yllcorner), cellsize (cell resolution) and nodata value."""
 PT_path_input = r'C:\Users\Mouris\Desktop\Test_Snow\Test_Codes\Results\PT_CSV_per_cell'
 original_columns = 170
 original_rows = 115
@@ -106,36 +98,38 @@ yllcorner = 4445000.000000
 cellsize = 1000
 nodata = -9999.0
 
-"""
-If run_rain_snow_rasters = True: 
-    - T_snow: Minimum temperature threshold to detect snowfall, very low value means the whole precipitation is considered as rain.
-"""
+""" If run_rain_snow_rasters = True: 
+- T_snow: float with minimum temperature threshold to detect snowfall, very low value means the whole precipitation is 
+    considered as rain."""
 T_snow = 0
 
-"""
-If run_rain_snow_rasters = False: (AND run_snow_melt is True OR run_r_factor is True) 
-    - snow_raster: Folder path where .tif snow rasters are located (needed for snow_melt calculation)
-    - rain_raster: Folder path where .tif rain rasters are located (needed for R factor calculation)
-"""
+""" If run_rain_snow_rasters = False: (AND run_snow_melt is True OR run_r_factor is True) 
+- snow_raster: Folder path where .tif snow rasters are located (needed for snow_melt calculation)
+- rain_raster: Folder path where .tif rain rasters are located (needed for R factor calculation)"""
 # snow_raster_input = r'C:\Users\Mouris\Desktop\Test_Snow\Test_Codes\Results\snow_per_month'
 snow_raster_input = r'Y:\Abt1\hiwi\Oreamuno\Tasks\Snow_Codes\Modifications_MF\Results\snow_per_month'
 rain_raster_input = r'\\LWW-027\Data\Abt1\hiwi\Oreamuno\Tasks\Snow_Codes\Modifications_MF\Results\rain_per_month'
 
-"""
-If "run_snow_cover" = True: 
-    - si_folder_path:  Folder with satellite images for a given date: folder must have a subfolder for each satellite 
-        (e.g. one for TDL and one for TDK), The input files can be raw satellite images (located in folder 'image_
-        _location_folder_name') or pre-processed (clipped and merged) .tif files. 
-    - input_si_dates: Boolean that determines if the user sets the image dates to use or if the program calculates the 
-        dates automatically. if "True" the program uses the dates in si_image_dates. If "False" the program automatically uses... ??
-    - image_list: Name of bands to merge, clip and resample. The name in the list must correspond to the final suffix of 
-        the satellite image name. They must be in the following order: band02, band03, band04, band11, TCI.
-    - image_location_folder_name: Name of the folder in which the satellite images are directly located.
-    - NDSI_min: NDSI threshold for snow detection.
-    - blue_min: Blue band threshold for snow detection.
-"""
-#si_folder_path = r'Y:\Abt1\hiwi\Oreamuno\Tasks\Satellite_data\Downloaded_MF'
-si_folder_path = r'C:\Users\Mouris\Desktop\Test_Snow\Test_Codes\Results\SatelliteImages' # is to be used if there are already processed SI
+""" If "run_snow_cover" = True: 
+- si_folder_path: Folder path where satellite images are: each folder must correspond to a sensing date, 
+    with the date in the name. The input files can be raw satellite images  or pre-processed (clipped and merged) 
+    .tif files. If raw images are given, then each folder must have a subfolder for each satellite analyzed (e.g. 
+    one for TDL and one for TDK), and the images must be located in a folder called 'image__location_folder_name'. 
+- input_si_dates: Boolean that determines if the user sets the image dates to use or if the program calculates the 
+    dates automatically. If "True" the program uses the dates in si_image_dates list. If "False" the program 
+    automatically uses the sensing date closest to the end of each month for all months. 
+- si_image_dates: LIST with sensing dates to use (in YYYYMMDD format) when input_si_dates is 'True'. The program 
+    determines to which month the input date corresponds. The user can set a sensing date for each month or just 
+    for some. For the months not set in the list, the program uses the date closest to the end of the month.
+- image_list: LIST with Name of bands to merge, clip and resample. The name in the list must correspond to the final 
+    suffix of the satellite image name. They must be in the following order: band02, band03, band04, band11, TCI.
+- image_location_folder_name: String with name of the folder in which the raw satellite images are directly located.
+    Only needed if 'run_satellite_image_clip_merge' = 'True'
+        
+- NDSI_min: float with NDSI threshold for snow detection.
+- blue_min: float with Blue band threshold for snow detection."""
+# si_folder_path = r'Y:\Abt1\hiwi\Oreamuno\Tasks\Satellite_data\Downloaded_MF'
+si_folder_path = r'C:\Users\Mouris\Desktop\Test_Snow\Test_Codes\Results\SatelliteImages'
 
 input_si_dates = True
 si_image_dates = [20161223]
@@ -149,15 +143,14 @@ blue_min = 1800
 
 """
 If "run_snow_cover" = False (and run_snow_melt is True)
-    - snowcover_raster_input: Folder path where .tif binary snow cover rasters are located.
-"""
+- snowcover_raster_input: Folder path where .tif binary snow cover rasters are located."""
 # snowcover_raster_input = r'C:\Users\Mouris\Desktop\Test_Snow\Test_Codes\Results\snow_cover'
 snowcover_raster_input = r'Y:\Abt1\hiwi\Oreamuno\Tasks\Snow_Codes\Modifications_MF\Results\snow_cover'
 
 """If "run_snow_melt" is True: 
 - plot_statistic: Boolean where if 'True', plots the snow melt statistics for a given input shapefile. 
 - shape_zone: Location of .shp file (can be different that clipping shape) in which to get snow melt statistics. 
-- statistic_param: Definition of statistical parameter to be plotted ('min', 'mean', 'max', 'range', 
+- statistic_param: string with definition of statistical parameter to be plotted ('min', 'mean', 'max', 'range', 
 'sum', 'coverage') """
 # Input for statistics
 # Disable (False) or enable (True) plot
@@ -170,29 +163,24 @@ statistical_param = 'coverage'
 # Output folder for plots (DO NOT MODIFY)
 plot_result = results_path + '\\Plots'
 
-"""
-If run_snow_melt is False (AND run_total_factor is True)
-    - snow_melt_input: folder where .tif snow melt rasters are located (each file name must contain the date).
-"""
+"""If run_snow_melt = False (AND run_total_factor = True)
+- snow_melt_input: folder path where .tif snow melt rasters are located (each file name must contain the date) """
 # snow_melt_input = r'C:\Users\Mouris\Desktop\Test_Snow\Test_Codes\Results\Snowmelt'
 snow_melt_input = r'C:\Users\Mouris\Desktop\Test_Snow\Test_Codes\Results\Snowmelt'
 
-"""
-if run_Rfactor is True: 
-    - fEl_path: Path where the F(E,EL) raster is, including raster name and extension (must be .tif).
+"""If run_Rfactor = True: 
+- fEl_path: Path where the f(E,EL) raster is, including raster name and extension (must be .tif).
 """
 fEL_path = r'' + os.path.abspath('..\Input\DEM\\f_L_E.tif')
 
-""" 
-if run_Rfactor is False (AND run_total_factor is True)
-    - r_factor_input: Folder path with .tif R factor rasters (with the raster name including the date).
+""" If run_Rfactor is False (AND run_total_factor is True)
+- r_factor_input: Folder path with .tif R factor rasters (with the raster name including the date).
 """
 # r_factor_input = r'C:\Users\Mouris\Desktop\Test_Snow\Test_Codes\Results\R_factor_REM_db'
 r_factor_input = r'Y:\Abt1\hiwi\Oreamuno\Tasks\Snow_Codes\Modifications_MF\Results\R_factor_REM_db'
 
-"""
-if run_total_factor is True: 
-    - snow_factor: Factor with which to multiply the snow melt raster values to get the snow melt erosivity.
+"""If run_total_factor is True: 
+- snow_factor: (float or int) Factor with which to multiply the snow melt raster values to get the snow melt erosivity.
 """
 snow_factor = 2
 
